3685f35f7b1728ef35c784d993913d96257f1df9
Marcos Cassiano <marcoscssno@gmail.com>
2023-05-03 16:08:19 -0300

    feat: check printer supplies service

---
+++ core/service/CheckPrinterSuppliesService.ts
@@ -0,0 +1,41 @@
+import puppeteer from 'puppeteer';
+import { PrinterSuppliesDTO } from './PrinterSuppliesDTO';
+
+class CheckPrinterSuppliesService {
+    constructor() { }
+    async execute(url: string): Promise<PrinterSuppliesDTO> {
+        try {
+            
+            const browser = await puppeteer.launch({ headless: "new", args: ['--proxy-server="192.168.14.1:3128" --proxy-bypass-list="192.168.14.*,192.168.8.*"'] });
+            const page = await browser.newPage();
+            await page.goto(url);
+            const toner = await page.waitForSelector('li#TonerSupplies > div > div > div.contentBody > div > div > div.progress-slider > span');
+            const tonerLevel = await toner?.evaluate(el => el.textContent)!;
+            const pcdrum = await page.waitForSelector('li#PCDrumStatus > div > div > div.contentBody > div > div > div.progress-slider > span');
+            const pcdrumLevel = await pcdrum?.evaluate(el => el.textContent)!;
+            const fuser = await page.waitForSelector('li#FuserSuppliesStatus > div > div > div.contentBody > div > div > div.progress-slider > span');
+            const fuserLevel = await fuser?.evaluate(el => el.textContent)!;
+            await browser.close();
+            return {
+                tonerLevel,
+                pcdrumLevel,
+                fuserLevel,
+                error: false,
+                errorMessage: null,
+            }
+        }
+        catch (error) {
+            return {
+                tonerLevel: null,
+                pcdrumLevel: null,
+                fuserLevel: null,
+                error: true,
+                errorMessage: error.message,
+            }
+        }
+    }
+}
+
+export {
+    CheckPrinterSuppliesService
+}
