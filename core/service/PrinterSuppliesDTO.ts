ab9740d21979e0edbe7816b924d863797a570833
Marcos Cassiano <marcoscssno@gmail.com>
2023-05-03 16:08:03 -0300

    feat: DTO for printer supplies

---
+++ core/service/PrinterSuppliesDTO.ts
@@ -0,0 +1,11 @@
+type PrinterSuppliesDTO = {
+    tonerLevel: string | null,
+    pcdrumLevel: string | null,
+    fuserLevel: string | null,
+    error: boolean,
+    errorMessage: string | null,
+}
+
+export {
+    PrinterSuppliesDTO
+}
