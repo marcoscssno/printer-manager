836309676bb993e74ba14378af66c8b55c985289
Marcos Cassiano <marcoscssno@gmail.com>
2023-05-04 12:16:14 -0300

    chore: change default url

--- index.ts
+++ index.ts
@@ -2,6 +2,6 @@ import { CheckPrinterSuppliesService } from "./core/service/CheckPrinterSupplies
 
 (async () => {
     const checkPrinterSuppliesService = new CheckPrinterSuppliesService();
-    const supplies = await checkPrinterSuppliesService.execute('http://192.168.8.15');
+    const supplies = await checkPrinterSuppliesService.execute('http://192.168.14.15');
     console.log(supplies);
 })();
