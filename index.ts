541c8bcd512c410a497077d13d77fd2c0065f8f7
Marcos Cassiano <marcoscssno@gmail.com>
2023-05-08 10:41:51 -0300

    feat: register data do mongodb

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
+import { MongoDBPrinterSupplyVerification } from "./core/repository/implementation/MongoDBPrinterSupplyVerification";
 import { CheckPrinterSuppliesService } from "./core/service/CheckPrinterSuppliesService";
 
 (async () => {
     const checkPrinterSuppliesService = new CheckPrinterSuppliesService();
     const supplies = await checkPrinterSuppliesService.execute('http://192.168.14.15');
     console.log(supplies);
+    const mongoDBPrinterSupplyVerification = new MongoDBPrinterSupplyVerification();
+    mongoDBPrinterSupplyVerification.register(supplies);
 })();
