14c502196e43fc9895ffe08304a0a157a9ad2494
Marcos Cassiano <marcoscssno@gmail.com>
2023-05-08 14:00:50 -0300

    chore: rename mongodb repository

---
+++ core/repository/implementation/MongoDBPrinterSupplyVerificationRepository.ts
@@ -0,0 +1,30 @@
+import { PrinterSuppliesDTO } from "../../service/PrinterSuppliesDTO";
+import { PrinterSupplyVerificationRepositoryInterface } from "../PrinterSupplyVerificationRepositoryInterface";
+import { mongoDBHelper } from "../../helpers/MongoDBHelper";
+
+class MongoDBPrinterSupplyVerificationRepository implements PrinterSupplyVerificationRepositoryInterface {
+    async register(data: PrinterSuppliesDTO): Promise<void> {
+        const client = mongoDBHelper.getClient();
+        try {
+            await client.db('printers').collection('printers').insertOne(data);
+        }
+        catch(error) {
+            console.error(error);
+        }
+    }
+    async findAll(): Promise<any[] | []> {
+        try {
+            const client = mongoDBHelper.getClient();
+            const printerSupplyVerifications = await client.db('printers').collection('printers').find().toArray();
+            return printerSupplyVerifications;
+        }
+        catch(error) {
+            console.error(error);
+            return [];
+        }
+    }   
+}
+
+export {
+    MongoDBPrinterSupplyVerificationRepository
+}
