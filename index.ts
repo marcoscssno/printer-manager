950eb337c44639f26f71bbb0473b6e2c6bdc0e68
Marcos Cassiano <marcoscssno@gmail.com>
2023-05-08 14:01:30 -0300

    refactor: connect to mongodb in main

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
-import { MongoDBPrinterSupplyVerification } from "./core/repository/implementation/MongoDBPrinterSupplyVerification";
+import { MongoDBPrinterSupplyVerificationRepository } from "./core/repository/implementation/MongoDBPrinterSupplyVerificationRepository";
 import { CheckPrinterSuppliesService } from "./core/service/CheckPrinterSuppliesService";
+import { mongoDBHelper } from './core/helpers/MongoDBHelper';
 
 (async () => {
+    await mongoDBHelper.connect();
     const checkPrinterSuppliesService = new CheckPrinterSuppliesService();
     const supplies = await checkPrinterSuppliesService.execute('http://192.168.14.15');
     console.log(supplies);
-    const mongoDBPrinterSupplyVerification = new MongoDBPrinterSupplyVerification();
-    mongoDBPrinterSupplyVerification.register(supplies);
+    const mongoDBPrinterSupplyVerificationRepository = new MongoDBPrinterSupplyVerificationRepository();
+    await mongoDBPrinterSupplyVerificationRepository.register(supplies);
 })();
