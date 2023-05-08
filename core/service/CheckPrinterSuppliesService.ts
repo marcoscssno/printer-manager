f7b1417195314d60c3f42c53d8fa780ee6abba52
Marcos Cassiano <marcoscssno@gmail.com>
2023-05-08 10:41:13 -0300

    refactor: update supply verification service

--- core/service/CheckPrinterSuppliesService.ts
+++ core/service/CheckPrinterSuppliesService.ts
@@ -4,8 +4,8 @@ import { PrinterSuppliesDTO } from './PrinterSuppliesDTO';
 class CheckPrinterSuppliesService {
     constructor() { }
     async execute(url: string): Promise<PrinterSuppliesDTO> {
+        const date = new Date();
         try {
-            
             const browser = await puppeteer.launch({ headless: "new", args: ['--proxy-server="192.168.14.1:3128" --proxy-bypass-list="192.168.14.*,192.168.8.*"'] });
             const page = await browser.newPage();
             await page.goto(url);
@@ -17,20 +17,24 @@ class CheckPrinterSuppliesService {
             const fuserLevel = await fuser?.evaluate(el => el.textContent)!;
             await browser.close();
             return {
+                printerId: url,
                 tonerLevel,
                 pcdrumLevel,
                 fuserLevel,
+                checkedAt: date,
                 error: false,
                 errorMessage: null,
             }
         }
         catch (error) {
             return {
+                printerId: url,
                 tonerLevel: null,
                 pcdrumLevel: null,
                 fuserLevel: null,
-                error: true,
-                errorMessage: error.message,
+                checkedAt: date,
+                error: false,
+                errorMessage: null,
             }
         }
     }
