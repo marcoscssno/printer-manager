ccf8ce4af9ace63952ad6bbe0b22900d23db5a95
Marcos Cassiano <marcoscssno@gmail.com>
2023-05-10 09:19:07 -0300

    feat: printer repository interface

---
+++ core/repository/PrinterRepositoryInterface.ts
@@ -0,0 +1,13 @@
+import { Printer } from "../entity/Printer";
+
+interface PrinterRepositoryInterface {
+    save(printer: Printer): Promise<void>;
+    findById(id: string): Promise<Printer>;
+    findAll(): Promise<Printer[] | []>;
+    update(id: string, printer: Printer): Promise<void>;
+    delete(id: string): Promise<void>;
+}
+
+export {
+    PrinterRepositoryInterface
+}
