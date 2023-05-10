6e2dca74c3518455d9c32b4ac9dd48df905f6339
Marcos Cassiano <marcoscssno@gmail.com>
2023-05-10 09:18:31 -0300

    feat: printer supply entity

---
+++ core/entity/PrinterSupply.ts
@@ -0,0 +1,15 @@
+class PrinterSupply {
+    private readonly id: string;
+    private name;
+    private level;
+    private createdAt: Date;
+    private createdBy: string;
+    private lastUpdatedAt: Date;
+    private lastUpdatedBy: string;
+    private isDeleted: boolean;
+    private deletedAt: Date;
+}
+
+export {
+    PrinterSupply
+}
