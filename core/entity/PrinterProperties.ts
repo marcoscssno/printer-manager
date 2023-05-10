34e94aeedd06b8e22b109b25ad62faf99e705620
Marcos Cassiano <marcoscssno@gmail.com>
2023-05-10 09:18:17 -0300

    feat: printer props

---
+++ core/entity/PrinterProperties.ts
@@ -0,0 +1,21 @@
+import { PrinterSupply } from "./PrinterSupply"
+
+interface PrinterProperties {
+    id?: string,
+    ipAddress?: string | null,
+    manufacturer: string,
+    model: string,
+    serialNumber?: string | null,
+    propertyNumber?: string | null,
+    supply?: PrinterSupply[] | null,
+    createdAt: Date,
+    createdBy: string,
+    lastUpdatedAt: Date,
+    lastUpdatedBy: string,
+    isDeleted: boolean,
+    deletedAt: Date | null
+}
+
+export {
+    PrinterProperties
+}
