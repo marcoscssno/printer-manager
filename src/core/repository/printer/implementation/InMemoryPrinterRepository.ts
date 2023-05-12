a8d2c5fb4a36755a2f6504cfb3121a9bf779ab0a
Marcos Cassiano <marcoscssno@gmail.com>
2023-05-12 16:55:16 -0300

    feat: printer in memory repository impl

---
+++ src/core/repository/printer/implementation/InMemoryPrinterRepository.ts
@@ -0,0 +1,51 @@
+import { Printer } from "@entity/printer/Printer";
+import { PrinterRepositoryInterface } from "../PrinterRepositoryInterface";
+
+class InMemoryPrinterRepository implements PrinterRepositoryInterface {
+    private printers: Printer[] = [];
+    constructor() { }
+    async save(printer: Printer): Promise<void> {
+        this.printers.push(printer);
+    }
+    async findById(id: string): Promise<Printer | undefined> {
+        const printer = this.printers.find(printer => printer.getId() === id);
+        return printer;
+    }
+    async findAll(): Promise<Printer[] | []> {
+        return this.printers;
+    }
+    async update(id: string, printer: Printer): Promise<void> {
+        const targetPrinter = this.printers.find(printer => printer.getId() === id);
+        if (targetPrinter) {
+            targetPrinter?.setIpAddress(printer.getIpAddress());
+            targetPrinter?.setManufacturer(printer.getManufacturer());
+            targetPrinter?.setModel(printer.getModel());
+            targetPrinter?.setSerialNumber(printer.getSerialNumber());
+            targetPrinter?.setPropertyNumber(printer.getPropertyNumber());
+            targetPrinter?.setSupply(printer.getSupply());
+            targetPrinter?.setCreatedAt(printer.getCreatedAt());
+            targetPrinter?.setCreatedBy(printer.getCreatedBy());
+            targetPrinter?.setLastUpdatedAt(printer.getLastUpdatedAt());
+            targetPrinter?.setLastUpdatedBy(printer.getLastUpdatedBy());
+            targetPrinter?.setIsDeleted(printer.getIsDeleted());
+            targetPrinter?.setDeletedAt(printer.getDeletedAt());
+        }
+    }
+    async delete(id: string): Promise<void> {
+        const targetPrinter = printer => printer.getId() === id;
+        const targetPrinterIndex = this.printers.findIndex(targetPrinter);
+        const targetPrinterExists = targetPrinterIndex >= 0;
+        if (targetPrinterExists) {
+            this.printers.splice(targetPrinterIndex, 1);
+        }
+    }
+}
+
+// Create a single instance of this repository for testing the API
+// Otherwise, it's data won't be available for different use cases.
+const inMemoryPrinterRepository = new InMemoryPrinterRepository();
+
+export {
+    InMemoryPrinterRepository,
+    inMemoryPrinterRepository
+}
