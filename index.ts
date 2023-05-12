c38f6b70ad13cae78e09ad5c4b75f67186de0d90
Marcos Cassiano <marcoscssno@gmail.com>
2023-05-12 16:25:18 -0300

    refactor: inject monbodb client in repository

--- index.ts
+++ index.ts
@@ -4,9 +4,10 @@ import { mongoDBHelper } from './core/helper/MongoDBHelper';
 
 (async () => {
     await mongoDBHelper.connect();
+    const client = mongoDBHelper.getClient();
     const checkPrinterSuppliesService = new CheckPrinterSuppliesService();
     const supplies = await checkPrinterSuppliesService.execute('http://192.168.14.15');
     console.log(supplies);
-    const mongoDBPrinterSupplyVerificationRepository = new MongoDBPrinterSupplyVerificationRepository();
+    const mongoDBPrinterSupplyVerificationRepository = new MongoDBPrinterSupplyVerificationRepository(client);
     await mongoDBPrinterSupplyVerificationRepository.register(supplies);
 })();
