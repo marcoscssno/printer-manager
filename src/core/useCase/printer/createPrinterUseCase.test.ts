0b30190e2224848c2db21c0e2f90edd0504aaeac
Marcos Cassiano <marcoscssno@gmail.com>
2023-05-29 16:18:37 -0300

    test: refactor create printer test

--- src/core/useCase/printer/createPrinterUseCase.test.ts
+++ src/core/useCase/printer/createPrinterUseCase.test.ts
@@ -13,16 +13,12 @@ describe('It should create a printer', () => {
     it('should create a printer', async () => {
         // Arrange
         const sut = makeSut();
-        const data = fakePrinter;
+        const data = fakePrinter();
         const allPrinters = await inMemoryPrinterRepository.findAll();
         // Act
         await sut.execute(data);
+        const printerFromRepository = allPrinters[0];
         // Assert
-        expect(allPrinters[0]).toHaveProperty('ipAddress', fakePrinter.ipAddress);
-        expect(allPrinters[0]).toHaveProperty('manufacturer', fakePrinter.manufacturer);
-        expect(allPrinters[0]).toHaveProperty('model', fakePrinter.model);
-        expect(allPrinters[0]).toHaveProperty('serialNumber', fakePrinter.serialNumber);
-        expect(allPrinters[0]).toHaveProperty('propertyNumber', fakePrinter.propertyNumber);
-        expect(allPrinters[0]).toHaveProperty('supply', fakePrinter.supply);
+        expect(printerFromRepository).toMatchObject(fakePrinter);
     });
 });
