d415c832bf72eb4e12e4015c1ae789fd5c99a46d
Marcos Cassiano <marcoscssno@gmail.com>
2023-05-29 13:16:43 -0300

    test: refactor create printer test

--- src/core/useCase/printer/createPrinterUseCase.test.ts
+++ src/core/useCase/printer/createPrinterUseCase.test.ts
@@ -1,8 +1,6 @@
 import { InMemoryPrinterRepository } from "@repository/printer/implementation/InMemoryPrinterRepository";
 import { CreatePrinterUseCase } from "./createPrinterUseCase";
-import { CreatePrinterRequestDTO } from "./createPrinterRequestDTO";
-import { faker } from '@faker-js/faker';
-import { PrinterSupply } from "@entity/printerSupply/PrinterSupply";
+import { fakePrinter } from "@shared/fake/fakePrinter";
 
 const inMemoryPrinterRepository = new InMemoryPrinterRepository();
 
@@ -15,41 +13,16 @@ describe('It should create a printer', () => {
     it('should create a printer', async () => {
         // Arrange
         const sut = makeSut();
-        const ipAddress = faker.internet.ipv4();
-        const manufacturer = faker.company.name();
-        const model = faker.string.alpha({ casing: 'upper', length: 3 }) + faker.string.numeric(3);
-        const serialNumber = faker.string.alphanumeric({ casing: 'upper', length: 10 });
-        const propertyNumber = faker.string.numeric(6)
-        const printerSupplyData = {
-            id: faker.string.uuid,
-            name: 'Toner',
-            level: faker.string.numeric(2),
-            createdAt: faker.date.recent(),
-            createdBy: faker.internet.userName(),
-            lastUpdatedAt: null,
-            lastUpdatedBy: null,
-            isDeleted: null,
-            deletedAt: null,
-        };
-        const toner = new PrinterSupply(printerSupplyData);
-        const supply = [ toner ]
-        const data: CreatePrinterRequestDTO = {
-            ipAddress,
-            manufacturer,
-            model,
-            serialNumber,
-            propertyNumber,
-            supply
-        }
+        const data = fakePrinter;
         const allPrinters = await inMemoryPrinterRepository.findAll();
         // Act
         await sut.execute(data);
         // Assert
-        expect(allPrinters[0]).toHaveProperty('ipAddress', ipAddress);
-        expect(allPrinters[0]).toHaveProperty('manufacturer', manufacturer);
-        expect(allPrinters[0]).toHaveProperty('model', model);
-        expect(allPrinters[0]).toHaveProperty('serialNumber', serialNumber);
-        expect(allPrinters[0]).toHaveProperty('propertyNumber', propertyNumber);
-        expect(allPrinters[0]).toHaveProperty('supply', supply);
+        expect(allPrinters[0]).toHaveProperty('ipAddress', fakePrinter.ipAddress);
+        expect(allPrinters[0]).toHaveProperty('manufacturer', fakePrinter.manufacturer);
+        expect(allPrinters[0]).toHaveProperty('model', fakePrinter.model);
+        expect(allPrinters[0]).toHaveProperty('serialNumber', fakePrinter.serialNumber);
+        expect(allPrinters[0]).toHaveProperty('propertyNumber', fakePrinter.propertyNumber);
+        expect(allPrinters[0]).toHaveProperty('supply', fakePrinter.supply);
     });
 });
