82389d4ce8ea3ece2b4d26438ea79cf39df57bb1
Marcos Cassiano <marcoscssno@gmail.com>
2023-05-29 12:26:12 -0300

    refactor: printer supply entity props

--- src/core/entity/printerSupply/PrinterSupply.ts
+++ src/core/entity/printerSupply/PrinterSupply.ts
@@ -1,13 +1,22 @@
+import { v4 as uuidv4 } from 'uuid';
+
 class PrinterSupply {
     private readonly id: string;
-    private name;
-    private level;
+    private name: string;
+    private level: string;
     private createdAt: Date;
     private createdBy: string;
     private lastUpdatedAt: Date;
     private lastUpdatedBy: string;
     private isDeleted: boolean;
     private deletedAt: Date;
+
+    constructor(props: Omit<PrinterSupply, 'id'>, id?: string) {
+        Object.assign(this, props);
+        if(!id) {
+            this.id = uuidv4();
+        }
+    }
 }
 
 export {
