62f8c4244e23e7206b6352665df0cb3bdb3e7134
Marcos Cassiano <marcoscssno@gmail.com>
2023-05-29 16:56:31 -0300

    test: refactor fake printer properties

--- src/core/shared/fake/fakeProp.ts
+++ src/core/shared/fake/fakeProp.ts
@@ -1,14 +1,14 @@
 import { faker } from '@faker-js/faker';
 
-const fakeIpAddress = faker.internet.ipv4();
-const fakeManufacturer = faker.company.name();
-const fakeModel = faker.string.alpha({ casing: 'upper', length: 3 }) + faker.string.numeric(3);
-const fakeSerialNumber = faker.string.alphanumeric({ casing: 'upper', length: 12 });
-const fakePropertyNumber = faker.string.numeric(6);
-const fakeId = faker.string.uuid();
-const fakeLevel = faker.string.numeric(2);
-const fakeRecentDate = faker.date.recent();
-const fakeUserName = (faker.person.firstName() + '.' + faker.person.lastName()).toLowerCase();
+const fakeIpAddress = () => faker.internet.ipv4();
+const fakeManufacturer = () => faker.company.name();
+const fakeModel = () => faker.string.alpha({ casing: 'upper', length: 3 }) + faker.string.numeric(3);
+const fakeSerialNumber = () => faker.string.alphanumeric({ casing: 'upper', length: 12 });
+const fakePropertyNumber = () => faker.string.numeric(6);
+const fakeId = () => faker.string.uuid();
+const fakeLevel = () => faker.string.numeric(2);
+const fakeRecentDate = () => faker.date.recent();
+const fakeUserName = () => (faker.person.firstName() + '.' + faker.person.lastName()).toLowerCase();
 
 export {
     fakeIpAddress,
