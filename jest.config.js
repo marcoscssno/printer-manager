b3c125a616f9ef66a439535a932ec73050382951
Marcos Cassiano <marcoscssno@gmail.com>
2023-05-29 11:29:46 -0300

    chore: map paths in jest config

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,11 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@entity/(.*)$': '<rootDir>/src/core/entity/$1',
+    '^@helper/(.*)$': '<rootDir>/src/core/helper/$1',
+    '^@repository/(.*)$': '<rootDir>/src/core/repository/$1',
+    '^@service/(.*)$': '<rootDir>/src/core/service/$1',
+    '^@useCase/(.*)$': '<rootDir>/src/core/useCase/$1',
+  },
 };
