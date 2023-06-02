ce7c5a8c12e78dc04a8bdc2de30acde133c65b21
Marcos Cassiano <marcoscssno@gmail.com>
2023-06-02 11:02:51 -0300

    style: rename PrinterProps

---
+++ src/core/entity/printer/PrinterProps.ts
@@ -0,0 +1,22 @@
+import { InferType } from "yup"
+import { PrinterSupply } from "../printerSupply/PrinterSupply"
+import { printerSchema } from "./printerSchema"
+
+interface PrinterProps {
+    readonly id: string,
+    ipAddress?: string | null,
+    manufacturer: string,
+    model?: string | null,
+    serialNumber?: string | null,
+    propertyNumber?: number | null,
+    createdAt: Date,
+    createdBy: string,
+    lastUpdatedAt?: Date | null,
+    lastUpdatedBy?: string | null,
+    isDeleted: boolean,
+    deletedAt?: Date | null
+}
+
+export {
+    PrinterProps
+}
