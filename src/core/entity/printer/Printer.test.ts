c85d23a8ee33d5fb2bc8d81075e9525a768fd8b2
Marcos Cassiano <marcoscssno@gmail.com>
2023-06-02 12:29:33 -0300

    test: ensure it assigns given id

--- src/core/entity/printer/Printer.test.ts
+++ src/core/entity/printer/Printer.test.ts
@@ -81,4 +81,24 @@ describe('Printer Entity', () => {
         // Assert
         expect(newPrinter.getId()).toBeDefined();
     });
+    it('should instantiate a printer entity', () => {
+        // Arrange
+        const metaData = {
+            createdAt: new Date(),
+            createdBy: 'user',
+            lastUpdatedAt: null,
+            lastUpdatedBy: null,
+            isDeleted: false,
+            deletedAt: null
+        }
+        const data = {
+            ...metaData,
+            ...fakePrinter()
+        }
+        const customId = 'f8211523-14a1-4a0b-b263-c6d0eec01c51';
+        // Act
+        const newPrinter = new Printer(data, customId);
+        // Assert
+        expect(newPrinter.getId()).toBe(customId);
+    });
 });
