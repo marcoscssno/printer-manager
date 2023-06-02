0ed265608d6bb34f37d5aa17560f484dcf36b878
Marcos Cassiano <marcoscssno@gmail.com>
2023-06-02 11:03:07 -0300

    feat: yup printer schema

---
+++ src/core/entity/printer/printerSchema.ts
@@ -0,0 +1,22 @@
+import { object, string, boolean, number, date, array, ObjectSchema } from 'yup';
+import { printerSupplySchema } from '@entity/printerSupply/printerSupplySchema';
+import { PrinterProps } from './PrinterProps';
+
+const printerSchema: ObjectSchema<PrinterProps> = object({
+    id: string().uuid().required(),
+    ipAddress: string().nullable().optional(),
+    manufacturer: string().required(),
+    model: string().nullable().optional(),
+    serialNumber: string().nullable().optional(),
+    propertyNumber: number().nullable().optional(),
+    createdAt: date().required(),
+    createdBy: string().required(),
+    lastUpdatedAt: date().nullable().optional(),
+    lastUpdatedBy: string().nullable().optional(),
+    isDeleted: boolean().required(),
+    deletedAt: date().nullable().optional()
+});
+
+export { 
+    printerSchema
+}
