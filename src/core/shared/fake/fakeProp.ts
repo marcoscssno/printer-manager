78d679bc982b3f799867d06c7ca0afeb114909de
Marcos Cassiano <marcoscssno@gmail.com>
2023-06-02 11:05:05 -0300

    refactor: make fake property number six digits

--- src/core/shared/fake/fakeProp.ts
+++ src/core/shared/fake/fakeProp.ts
@@ -4,7 +4,7 @@ const fakeIpAddress = () => faker.internet.ipv4();
 const fakeManufacturer = () => faker.company.name();
 const fakeModel = () => faker.string.alpha({ casing: 'upper', length: 3 }) + faker.string.numeric(3);
 const fakeSerialNumber = () => faker.string.alphanumeric({ casing: 'upper', length: 12 });
-const fakePropertyNumber = () => faker.string.numeric(6);
+const fakePropertyNumber = () => faker.number.int({ min: 100000, max: 999999 });
 const fakeId = () => faker.string.uuid();
 const fakeLevel = () => faker.string.numeric(2);
 const fakeRecentDate = () => faker.date.recent();
