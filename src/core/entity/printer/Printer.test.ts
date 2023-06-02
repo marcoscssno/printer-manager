90dd1c21b57cacda40bcbdb14e332b960db5641d
Marcos Cassiano <marcoscssno@gmail.com>
2023-06-02 11:06:04 -0300

    test: ensure printer entity can be instantiated

---
+++ src/core/entity/printer/Printer.test.ts
@@ -0,0 +1,25 @@
+import { Printer } from "./Printer";
+import { fakePrinter } from "@shared/fake/fakePrinter";
+
+describe('Printer Entity', () => {
+    it('should instantiate a printer entity', async () => {
+        // Arrange
+        const metaData = {
+            createdAt: new Date(),
+            createdBy: 'user',
+            lastUpdatedAt: null,
+            lastUpdatedBy: null,
+            isDeleted: false,
+            deletedAt: null
+        }
+        const data = {
+            ...metaData,
+            ...fakePrinter()
+        }
+        const customId = 'f8211523-14a1-4a0b-b263-c6d0eec01c51';
+        // Act
+        const newPrinter = new Printer(data, customId);
+        // Assert
+        expect(newPrinter.getId()).toBe(customId);
+    });
+});
