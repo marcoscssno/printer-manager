cf9422cc0340a359c3b0f59a3ee7f8356fcd4500
Marcos Cassiano <marcoscssno@gmail.com>
2023-06-02 11:04:43 -0300

    refactor: remove printer supply from fake printer

--- src/core/shared/fake/fakePrinter.ts
+++ src/core/shared/fake/fakePrinter.ts
@@ -1,43 +1,4 @@
-import { PrinterSupply } from '@entity/printerSupply/PrinterSupply';
 import { fakeIpAddress, fakeManufacturer, fakeModel, fakeSerialNumber, fakePropertyNumber, fakeId, fakeLevel, fakeRecentDate, fakeUserName } from './fakeProp';
-
-const tonerData = {
-    id: fakeId(),
-    name: 'Toner',
-    level: fakeLevel(),
-    createdAt: fakeRecentDate(),
-    createdBy: fakeUserName(),
-    lastUpdatedAt: null,
-    lastUpdatedBy: null,
-    isDeleted: null,
-    deletedAt: null,
-};
-const toner = new PrinterSupply(tonerData);
-const drumData = {
-    id: fakeId(),
-    name: 'Drum',
-    level: fakeLevel(),
-    createdAt: fakeRecentDate(),
-    createdBy: fakeUserName(),
-    lastUpdatedAt: null,
-    lastUpdatedBy: null,
-    isDeleted: null,
-    deletedAt: null,
-};
-const drum = new PrinterSupply(drumData);
-const fuserData = {
-    id: fakeId(),
-    name: 'Fuser',
-    level: fakeLevel(),
-    createdAt: fakeRecentDate(),
-    createdBy: fakeUserName(),
-    lastUpdatedAt: null,
-    lastUpdatedBy: null,
-    isDeleted: null,
-    deletedAt: null,
-};
-const fuser = new PrinterSupply(fuserData);
-const supply = [toner, drum, fuser]
 
 const fakePrinter = () => {
     return {    
@@ -45,8 +6,7 @@ const fakePrinter = () => {
         manufacturer: fakeManufacturer(),
         model: fakeModel(),
         serialNumber: fakeSerialNumber(),
-        propertyNumber: fakePropertyNumber(),
-        supply
+        propertyNumber: fakePropertyNumber()
     }
 }
 
