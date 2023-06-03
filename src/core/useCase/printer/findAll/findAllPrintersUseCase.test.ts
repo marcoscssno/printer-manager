9d2bd5b44d5a259f48e2ff9d55a04079e61e620d
Marcos Cassiano <marcoscssno@gmail.com>
2023-06-03 10:59:24 -0300

    test: ensure it returns printers from repository

---
+++ src/core/useCase/printer/findAll/findAllPrintersUseCase.test.ts
@@ -0,0 +1,33 @@
+import { Printer } from "@entity/printer/Printer";
+import { InMemoryPrinterRepository } from "@repository/printer/implementation/InMemoryPrinterRepository";
+import { fakePrinter } from "@shared/fake/fakePrinter";
+import { FindAllPrintersUseCase } from "./findAllPrintersUseCase";
+
+const inMemoryPrinterRepository = new InMemoryPrinterRepository();
+
+const makeSut = () => {
+    const sut = new FindAllPrintersUseCase(inMemoryPrinterRepository);
+    return sut;
+};
+
+describe('Find All Printers Use Case', () => {
+    it('should return all printers in repository', async () => {
+        const sut = makeSut();
+        const metaData = {
+            createdAt: new Date(),
+            createdBy: 'user',
+            isDeleted: false
+        }
+        const firstPrinterData = { ...fakePrinter(), ...metaData };
+        const firstPrinter = new Printer(firstPrinterData);
+        const secondPrinterData = { ...fakePrinter(), ...metaData };
+        const secondPrinter = new Printer(secondPrinterData);
+        
+        await inMemoryPrinterRepository.save(firstPrinter);
+        await inMemoryPrinterRepository.save(secondPrinter);
+        const allPrinters = await sut.execute();
+        
+        expect(allPrinters[0]).toBe(firstPrinter);
+        expect(allPrinters[1]).toBe(secondPrinter);
+    });
+});
