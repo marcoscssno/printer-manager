ce42de38b524b55fc092ed66322f30909f16437b
Marcos Cassiano <marcoscssno@gmail.com>
2023-06-04 14:41:40 -0300

    feat: find printer by id controller

---
+++ src/core/useCase/printer/findById/findPrinterByIdController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import { FindPrinterByIdUseCase } from "./findPrinterByIdUseCase";
+
+class FindPrinterByIdController {
+    constructor(
+        private findPrinterByIdUseCase: FindPrinterByIdUseCase
+    ) { }
+    async handle(request: Request, response: Response): Promise<Response> {
+        try {
+            const { id } = request.params;
+            const printer = await this.findPrinterByIdUseCase.execute(id);
+            return response.status(200).json(printer);
+        }
+        catch (error) {
+            return response.status(400).json({
+                message: error.message || 'Unexpected error'
+            });
+        }
+    }
+}
+
+export {
+    FindPrinterByIdController
+}
