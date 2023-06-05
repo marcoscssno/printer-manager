aa4ee4b9b2e28de9155072dceff89ce15c28d3c2
Marcos Cassiano <marcoscssno@gmail.com>
2023-06-04 22:10:41 -0300

    feat: update printer route

--- src/core/routes.ts
+++ src/core/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { createPrinterController } from "@useCase/printer/create";
 import { findAllPrintersController } from "@useCase/printer/findAll";
 import { findPrinterByIdController } from "@useCase/printer/findById";
+import { updatePrinterController } from "@useCase/printer/update";
 
 const router = Router()
 
@@ -17,6 +18,9 @@ router.get('/printer/:id', (request, response) => {
     return findPrinterByIdController.handle(request, response);
 });
 
+router.put('/printer/:id', (request, response) => {
+    return updatePrinterController.handle(request, response);
+});
 
 export {
     router
