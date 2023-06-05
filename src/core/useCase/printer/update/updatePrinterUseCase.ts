24e7ab15aa48f6a6324eef9378a854f3e8e4b262
Marcos Cassiano <marcoscssno@gmail.com>
2023-06-04 22:10:13 -0300

    feat: update printer use case

---
+++ src/core/useCase/printer/update/updatePrinterUseCase.ts
@@ -0,0 +1,41 @@
+import { PrinterRepositoryInterface } from "@repository/printer/PrinterRepositoryInterface";
+import { CreatePrinterRequestDTO } from "@useCase/printer/create/createPrinterRequestDTO";
+import { Printer } from "@entity/printer/Printer";
+
+class UpdatePrinterUseCase {
+    constructor(
+        private printerRepository: PrinterRepositoryInterface
+    ) {}
+    async execute(id, data: CreatePrinterRequestDTO): Promise<void> {
+        const printerExists = await this.printerRepository.findById(id);
+        if(!printerExists) {
+            throw new Error(`Printer with id ${id} was not found`);
+        }
+        const { ipAddress, manufacturer, model, serialNumber, propertyNumber } = data;
+        const createdAt = printerExists.createdAt;
+        const createdBy = printerExists.createdBy;
+        const lastUpdatedAt = new Date();
+        const lastUpdatedBy = 'user';
+        const isDeleted = false;
+        const deletedAt = null;
+        const printerProps = {
+            ipAddress,
+            manufacturer,
+            model,
+            serialNumber,
+            propertyNumber,
+            createdAt,
+            createdBy,
+            lastUpdatedAt,
+            lastUpdatedBy,
+            isDeleted,
+            deletedAt,
+        }
+        const printer: Printer = new Printer(printerProps, id);
+        await this.printerRepository.update(id, printer.getProps());
+    }
+}
+
+export {
+    UpdatePrinterUseCase
+}
