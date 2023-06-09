27f3c1df13040b7ca64eb12d34f6c291bfe73a7c
Marcos Cassiano <marcoscssno@gmail.com>
2023-06-08 23:47:32 -0300

    test: update printer supply type use case

---
+++ src/core/useCase/printerSupplyType/update/updatePrinterSupplyTypeUseCase.test.ts
@@ -0,0 +1,40 @@
+import { InMemoryPrinterSupplyTypeRepository } from "@repository/printerSupplyType/implementation/InMemoryPrinterSupplyTypeRepository";
+import { UpdatePrinterSupplyTypeUseCase } from "@useCase/printerSupplyType/update/updatePrinterSupplyTypeUseCase";
+import { fakePrinterSupplyType } from "@shared/fake/fakePrinterSupplyType";
+import { PrinterSupplyType } from "@entity/printerSupplyType/PrinterSupplyType";
+import { fakeMetaData } from "@shared/fake/fakeMetaData";
+
+const inMemoryPrinterSupplyTypeRepository = new InMemoryPrinterSupplyTypeRepository();
+
+const makeSut = () => {
+    const sut = new UpdatePrinterSupplyTypeUseCase(inMemoryPrinterSupplyTypeRepository);
+    return sut;
+};
+
+describe('It should update a printer supply type', () => {
+    it('should throw error if printer supply type is not found', async () => {
+        const sut = makeSut();
+        const id = 'someId';
+        const metaData = fakeMetaData();
+        const printerSupplyTypeData = { ...fakePrinterSupplyType(), ...metaData };
+        const printerSupplyType = new PrinterSupplyType(printerSupplyTypeData);
+        const printerSupplyTypeProps = printerSupplyType.getProps();
+        const updatePrinterSupplyType = async() => await sut.execute(id, printerSupplyTypeProps);
+
+        expect(updatePrinterSupplyType).rejects.toThrow();
+    });
+    it('should update a printer supply type', async () => {
+        const sut = makeSut();
+        const metaData = fakeMetaData();
+        const printerSupplyTypeData = { ...fakePrinterSupplyType(), ...metaData };
+        const printerSupplyType = new PrinterSupplyType(printerSupplyTypeData);
+        const id = printerSupplyType.getId();
+        await inMemoryPrinterSupplyTypeRepository.save(printerSupplyType.getProps());
+        const newName = 'Fuser';
+        const printerSupplyTypeFromRepository = await inMemoryPrinterSupplyTypeRepository.findById(id);
+
+        await sut.execute(id, { name: newName });
+        
+        expect(printerSupplyTypeFromRepository?.name).toBe(newName);
+    });
+});
