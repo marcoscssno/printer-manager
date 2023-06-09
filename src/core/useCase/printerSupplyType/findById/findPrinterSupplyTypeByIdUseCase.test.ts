c911dbc9618f592bb8ce7103d80d07f11bc20841
Marcos Cassiano <marcoscssno@gmail.com>
2023-06-08 23:46:27 -0300

    test: find printer supply type by id use case

---
+++ src/core/useCase/printerSupplyType/findById/findPrinterSupplyTypeByIdUseCase.test.ts
@@ -0,0 +1,28 @@
+import { PrinterSupplyType } from "@entity/printerSupplyType/PrinterSupplyType";
+import { InMemoryPrinterSupplyTypeRepository } from "@repository/printerSupplyType/implementation/InMemoryPrinterSupplyTypeRepository";
+import { fakeMetaData } from "@shared/fake/fakeMetaData";
+import { fakePrinterSupplyType } from "@shared/fake/fakePrinterSupplyType";
+import { FindPrinterSupplyTypeByIdUseCase } from "./findPrinterSupplyTypeByIdUseCase";
+
+const inMemoryPrinterSupplyTypeRepository = new InMemoryPrinterSupplyTypeRepository();
+
+const makeSut = () => {
+    const sut = new FindPrinterSupplyTypeByIdUseCase(inMemoryPrinterSupplyTypeRepository);
+    return sut;
+};
+
+describe('Find Printer Supply Type By Id Use Case', () => {
+    it('should find printer supply type by id', async () => {
+        const sut = makeSut();
+        const metaData = fakeMetaData();
+        const printerSupplyTypeData = { ...fakePrinterSupplyType(), ...metaData };
+        const printerSupplyType = new PrinterSupplyType(printerSupplyTypeData);
+        const id = printerSupplyType.getId();
+        const newPrinterSupplyType = printerSupplyType.getProps();
+
+        await inMemoryPrinterSupplyTypeRepository.save(printerSupplyType.getProps());
+        const printerSupplyTypeFromRepository = await sut.execute(id);
+
+        expect(printerSupplyTypeFromRepository).toBe(newPrinterSupplyType);
+    });
+});
