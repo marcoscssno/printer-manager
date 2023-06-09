f7bd79c9ebc8345179f2b5d3e6e9436fb37b9e09
Marcos Cassiano <marcoscssno@gmail.com>
2023-06-08 23:27:14 -0300

    chore: move routes to own subject file

--- src/core/routes/printerRouter.ts
+++ src/core/routes/printerRouter.ts
@@ -5,28 +5,28 @@ import { findPrinterByIdController } from "@useCase/printer/findById";
 import { updatePrinterController } from "@useCase/printer/update";
 import { deletePrinterController } from "@useCase/printer/delete";
 
-const router = Router()
+const printerRouter = Router()
 
-router.post('/printer', (request, response) => {
+printerRouter.post('/printer', (request, response) => {
     return createPrinterController.handle(request, response);
 });
 
-router.get('/printer', (request, response) => {
+printerRouter.get('/printer', (request, response) => {
     return findAllPrintersController.handle(request, response);
 });
 
-router.get('/printer/:id', (request, response) => {
+printerRouter.get('/printer/:id', (request, response) => {
     return findPrinterByIdController.handle(request, response);
 });
 
-router.put('/printer/:id', (request, response) => {
+printerRouter.put('/printer/:id', (request, response) => {
     return updatePrinterController.handle(request, response);
 });
 
-router.delete('/printer/:id', (request, response) => {
+printerRouter.delete('/printer/:id', (request, response) => {
     return deletePrinterController.handle(request, response);
 });
 
 export {
-    router
-}
+    printerRouter
+};
