2f296143a18fea436bc3a2cada828ec135fba102
Marcos Cassiano <marcoscssno@gmail.com>
2023-06-27 07:25:29 -0300

    refactor: connect to database

--- src/core/server.ts
+++ src/core/server.ts
@@ -1,4 +1,14 @@
 import { app } from "./app";
 import { PORT } from "@shared/environment";
+import { mongoDBHelper } from "@helper/MongoDBHelper";
+import { MONGODB_URI } from "@shared/environment";
 
-app.listen(PORT || 3000);
+(async () => {
+    try {
+        await mongoDBHelper.connect(MONGODB_URI);
+    }
+    catch (error) {
+        throw new Error(error);
+    }
+    app.listen(PORT || 3000);
+})();
